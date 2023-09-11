e523d2d0f106a8a36a1414e8a2f2e8764db586be
VasyliukAndriy <144329579+VasyliukAndriy@users.noreply.github.com>
2023-09-11 22:12:55 +0300

    Create Ce ya.js

---
+++ Ce ya.js
@@ -0,0 +1,16 @@
+let number = 15
+let b = NaN
+let Text = " Privet"
+let t = underfined
+
+
+let I am = {
+  Name: "Andriy"
+  Surname: "Vasyliuk"
+Age: "18"
+Town: "Tlumach"
+}
+
+console.log(number, b, Text, TrueFalse, t)
+console.table(I am)
+
