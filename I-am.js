69c49812454b1008bf6026ed187afe771696bc30
VasyliukAndriy <144329579+VasyliukAndriy@users.noreply.github.com>
2023-09-11 22:32:16 +0300

    Create I-am.js

---
+++ I-am.js
@@ -0,0 +1,15 @@
+let number = 19
+let b = NaN
+let Text = "Privet"
+let TrueFalse = true
+let t = undefined
+
+let I-am = {
+  Name: "Andriy",
+  Surname: "Vasyliuk",
+  Age: "18",
+  Town: "Tlumach"
+}
+
+console.log(number, b, Text, TrueFalse, t)
+console.table(I-am) 
